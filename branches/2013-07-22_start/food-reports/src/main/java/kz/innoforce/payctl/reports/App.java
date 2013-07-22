959981a0d93639aa833a602e0a6cd9be7a484688
zhandos22.03.90@gmail.com <zhandos22.03.90@gmail.com@9a655c1f-8ada-6002-8b15-3f1e31f2080f>
2013-07-22 14:33:56 +0000

    from trunk

---
+++ branches/2013-07-22_start/food-reports/src/main/java/kz/innoforce/payctl/reports/App.java
@@ -0,0 +1,12 @@
+package kz.innoforce.payctl.reports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
