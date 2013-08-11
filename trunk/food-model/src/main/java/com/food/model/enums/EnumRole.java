36023ed4a8959caec658ec47df1aff56d859dae4
zhandos22.03.90@gmail.com <zhandos22.03.90@gmail.com@9a655c1f-8ada-6002-8b15-3f1e31f2080f>
2013-08-11 17:42:16 +0000

    реализовал DAO

--- trunk/food-model/src/main/java/com/food/model/enums/EnumRole.java
+++ trunk/food-model/src/main/java/com/food/model/enums/EnumRole.java
@@ -2,8 +2,19 @@ package com.food.model.enums;
 
 public enum EnumRole {
 
-    ROLE_CLIENT,
-    ROLE_ADMIN,
-    ROLE_CONSUMER,
-    ROLE_ANONYMOUS
+    ROLE_CLIENT("РОЛЬ КЛИЕНТА"),
+    ROLE_ADMIN("РОЛЬ АДМИНСТРАТОРА"),
+    ROLE_CONSUMER("РОЛЬ КОМПАНЬОНА"),
+    ROLE_ANONYMOUS("НЕ АВТОРИЗОВАННЫЙ ПОЛЗОВАТЕЛЬ");
+    
+    EnumRole(String descriptin)
+    {
+        this.descriptin=descriptin;
+    }
+    
+    public String getDescription()
+    {
+        return this.descriptin;
+    }
+    private String descriptin;
 }
