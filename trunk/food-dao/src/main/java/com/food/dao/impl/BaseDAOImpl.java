60f89ee7519aeb8cc453c325c9bd1937c9f27dad
zhandos22.03.90@gmail.com <zhandos22.03.90@gmail.com@9a655c1f-8ada-6002-8b15-3f1e31f2080f>
2013-10-09 15:16:57 +0000

    реализовал страницу ресторанов

--- trunk/food-dao/src/main/java/com/food/dao/impl/BaseDAOImpl.java
+++ trunk/food-dao/src/main/java/com/food/dao/impl/BaseDAOImpl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,82 +24,83 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Repository
 public abstract class BaseDAOImpl<T, KeyType extends Serializable> implements BaseDAO<T, KeyType> {
-    
+
     @Autowired
     @Qualifier("sessionFactoryPostgres")
     protected SessionFactory sessionFactory;
     private Class<T> domainClass;
-    
+
     public BaseDAOImpl() {
         domainClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
-    
+
     protected Session ht() {
         return sessionFactory.getCurrentSession();
     }
-    
+
     @Override
     public T save(T entity) {
         ht().saveOrUpdate(entity);
         return entity;
     }
-    
+
     @Override
     public T update(T entity) {
         ht().saveOrUpdate(entity);
         return entity;
     }
-    
+
     @Override
     public void delete(T entity) {
         ht().delete(entity);
     }
-    
+
     @Override
     public T getById(KeyType id) {
         return (T) ht().get(domainClass, id);
     }
-    
+
     @Override
     public List<T> getAll() {
         return ht().createCriteria(domainClass).list();
     }
-    
+
     @Override
     public void saveAll(Collection<T> entities) {
         for (T t : entities) {
             ht().save(t);
         }
     }
-    
+
     @Override
     public void updateAll(Collection<T> entities) {
         for (T t : entities) {
             ht().update(t);
         }
     }
-    
+
     @Override
     public void deleteById(KeyType id) {
         Object o = getById(id);
         ht().delete(o);
     }
-    
+
     @Override
     @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
     public List<T> getAll(int maxResult, int firstResult) {
         return ht().createCriteria(domainClass)
+                .addOrder(Order.asc("id"))
                 .setFirstResult(firstResult)
                 .setMaxResults(maxResult).list();
     }
-    
+
     @Override
     public void deletelAllById(Collection<KeyType> ids) {
         for (KeyType id : ids) {
             deleteById(id);
         }
     }
-    
+
     @Override
     public void deleteAll(Collection<T> entities) {
         for (T t : entities) {
